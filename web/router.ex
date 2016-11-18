9244ac36f02d5fb4b2690dd7fe4d1a7a750ef4a2
iredsausage <antuanl@aol.com>
2016-11-18 16:23:04 +0100

    Begore Auth

---
+++ web/router.ex
@@ -0,0 +1,26 @@
+defmodule Rumbl.Router do
+  use Rumbl.Web, :router
+
+  pipeline :browser do
+    plug :accepts, ["html"]
+    plug :fetch_session
+    plug :fetch_flash
+    plug :protect_from_forgery
+    plug :put_secure_browser_headers
+  end
+
+  pipeline :api do
+    plug :accepts, ["json"]
+  end
+
+  scope "/", Rumbl do
+    pipe_through :browser
+    resources "/users", UserController, only: [:index, :show, :new, :create]
+    get "/", PageController, :index
+  end
+
+  # Other scopes may use custom stacks.
+  # scope "/api", Rumbl do
+  #   pipe_through :api
+  # end
+end
