13036662c1f4ae79c06ca050d4790654c59986b9
iredsausage <taorg@taorgs-MBP-2.Home>
2017-02-24 12:56:54 +0100

    Se reacciona al cambio de la lista desde drab

--- web/commanders/page_commander.ex
+++ web/commanders/page_commander.ex
@@ -40,13 +40,14 @@ alias ExGoogle.Maps.Api, as: Maps
     def gaddress_select_input(socket, dom_sender) do
         map_search =  Maps.search(%{address: dom_sender["val"]})
         map_address_map = gmaps_select (map_search)
-         
+
 
         socket
         |> update!(:html, set: map_address_map,  on: "#gmap-address-select")
-
+        # $('#gmap-address-select').prevAll('input.select-dropdown').val()
+        #$('#gmap-address-select').prevAll('input.select-dropdown').trigger('open')
         _= socket
-        |>execjs("$('select').material_select();")
+        |>execjs("$('#gmap-address-select').material_select();")
         socket
     end
 
@@ -75,5 +76,12 @@ alias ExGoogle.Maps.Api, as: Maps
     defp gmaps_flat(x) do
          x["formatted_address"]
     end
+
+    def gmap_address_select_changed(socket, dom_sender) do
+        IO.inspect dom_sender
+        ""
+        _= socket
+    end
+
 
 end
