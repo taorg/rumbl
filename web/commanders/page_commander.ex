6d680e92d31573d8c1d52ac2530d886ec2940e45
iredsausage <taorg@taorgs-MBP-2.Home>
2017-03-02 15:13:18 +0100

    Leak en los handlers del autocomplete eliminados

--- web/commanders/page_commander.ex
+++ web/commanders/page_commander.ex
@@ -1,4 +1,5 @@
 defmodule Rumbl.PageCommander do
+require Logger
 use Drab.Commander,modules: [Drab.Query]
 alias ExGoogle.Maps.Api, as: Maps
     def gaddress_autocomplete_input_changed(socket, dom_sender) do
@@ -83,5 +84,52 @@ alias ExGoogle.Maps.Api, as: Maps
         _= socket
     end
 
-
+####################################
+# FUSE AUTOCOMPLETE
+####################################
+def fuse_input_on_keyup(socket, dom_sender) do
+    Logger.debug "Dom sender: #{inspect dom_sender}"
+    fuse_input = dom_sender["val"]
+    case dom_sender |> get_in(["event", "which"]) do
+      any_other -> if fuse_input |> String.length > 1 do        
+        socket |> execjs(populate_js)
+      end
+    end
+    socket
+  end
+  def fuse_input_on_autocomplete(socket, dom_sender) do
+    Logger.debug "********* on_autocomplete: #{inspect dom_sender}"
+    #socket |> execjs(select_customer(fuse_input))
+    socket
+  end
+  defp populate_js do
+    """
+      $('#fuse-autocomplete-input').fuseautocomplete({          
+          data:[{title:"Old Man's War",author:{img:"/images/processing.gif",firstName:"John",lastName:"Scalzi"}},{title:"The Lock Artist",author:{img:"/images/processing.gif",firstName:"Steve",lastName:"Hamilton"}},{title:"HTML5",author:{img:"/images/processing.gif",firstName:"Remy",lastName:"Sharp"}},{title:"Right Ho Jeeves",author:{img:"/images/processing.gif",firstName:"P.D",lastName:"Woodhouse"}},{title:"The Code of the Wooster",author:{img:"/images/processing.gif",firstName:"P.D",lastName:"Woodhouse"}},{title:"Thank You Jeeves",author:{img:"/images/processing.gif",firstName:"P.D",lastName:"Woodhouse"}},{title:"The DaVinci Code",author:{img:"/images/processing.gif",firstName:"Dan",lastName:"Brown"}},{title:"Angels & Demons",author:{img:"/images/processing.gif",firstName:"Dan",lastName:"Brown"}},{title:"The Silmarillion",author:{img:"/images/processing.gif",firstName:"J.R.R",lastName:"Tolkien"}},{title:"Syrup",author:{img:"/images/processing.gif",firstName:"Max",lastName:"Barry"}},{title:"The Lost Symbol",author:{img:"/images/processing.gif",firstName:"Dan",lastName:"Brown"}},{title:"The Book of Lies",author:{img:"/images/processing.gif",firstName:"Brad",lastName:"Meltzer"}},{title:"Lamb",author:{img:"/images/processing.gif",firstName:"Christopher",lastName:"Moore"}},{title:"Fool",author:{img:"/images/processing.gif",firstName:"Christopher",lastName:"Moore"}},{title:"Incompetence",author:{img:"/images/processing.gif",firstName:"Rob",lastName:"Grant"}},{title:"Fat",author:{img:"/images/processing.gif",firstName:"Rob",lastName:"Grant"}},{title:"Colony",author:{img:"/images/processing.gif",firstName:"Rob",lastName:"Grant"}},{title:"Backwards, Red Dwarf",author:{img:"/images/processing.gif",firstName:"Rob",lastName:"Grant"}},{title:"The Grand Design",author:{img:"/images/processing.gif",firstName:"Stephen",lastName:"Hawking"}},{title:"The Book of Samson",author:{img:"/images/processing.gif",firstName:"David",lastName:"Maine"}},{title:"The Preservationist",author:{img:"/images/processing.gif",firstName:"David",lastName:"Maine"}},{title:"Fallen",author:{img:"/images/processing.gif",firstName:"David",lastName:"Maine"}},{title:"Monster 1959",author:{img:"/images/processing.gif",firstName:"David",lastName:"Maine"}}],
+          fuseOptions: {
+              //id: "title",
+              shouldSort: !0,
+              threshold: .6,
+              location: 0,
+              distance: 100,
+              maxPatternLength: 32,
+              minMatchCharLength: 1,
+              keys: [ "title", "firstName" ]
+            },             
+          dataIdentifierFn: function (fdata_key) {
+          var id =  fdata_key.author.lastName;          
+          return id;         
+          },         
+          renderItemFn: function (fdata_key) {
+          var image = '<img src="'+ fdata_key.author.img +'" class="right circle"><span>'+ fdata_key.title +'</span>'            
+          
+          return image;         
+          },
+          onAutocomplete:function(){ console.log('********ONAUTOCOMPLETE');
+                                    console.log($("#fuse-autocomplete-input").data("data-id"));
+                                    }
+  });
+  """
+  
+  end
 end
--- web/static/vendor/material-datetime-picker/fuseAutocomplete.js
+++ web/static/vendor/material-datetime-picker/fuseAutocomplete.js
@@ -1,168 +1,170 @@
 /**************************
  * Auto complete plugin  *
  *************************/
-(function ($) {
-$.fn.fuseautocomplete = function (options) {
-  // Defaults
-  var defaults = {
-    data: {},
-    limit: Infinity,
-    onAutocomplete: null
-  };
+(function($) {
+    $.fn.fuseautocomplete = function(options) {
+        // Defaults
+        var defaults = {
+            data: {},
+            limit: Infinity,
+            onAutocomplete: null
+        };
 
-  options = $.extend(defaults, options);
+        options = $.extend(defaults, options);
 
-  return this.each(function() {
-    var $input = $(this);
-    var data = options.data,
-        fuseOptions = options.fuseOptions,
-        dataIdentifierFn = options.dataIdentifierFn,
-        renderItemFn = options.renderItemFn,
-        fuse = new Fuse(data, fuseOptions),
-        count = 0,
-        activeIndex = 0,
-        oldVal,
-        $inputDiv = $input.closest('.input-field'); // Div to append on
+        return this.each(function() {
+            var $input = $(this);
+            var data = options.data,
+                fuseOptions = options.fuseOptions,
+                dataIdentifierFn = options.dataIdentifierFn,
+                renderItemFn = options.renderItemFn,
+                fuse = new Fuse(data, fuseOptions),
+                count = 0,
+                activeIndex = 0,
+                oldVal,
+                $inputDiv = $input.closest('.input-field'); // Div to append on
 
-    // Check if data isn't empty
-    if (!$.isEmptyObject(data)) {
-      var $autocomplete = $('<ul class="autocomplete-content dropdown-content"></ul>');
-      var $oldAutocomplete;
+            // Check if data isn't empty
+            if (!$.isEmptyObject(data)) {
+                var $autocomplete = $('<ul class="autocomplete-content dropdown-content"></ul>');
+                var $oldAutocomplete;
 
-      // Append autocomplete element.
-      // Prevent double structure init.
-      if ($inputDiv.length) {
-        $oldAutocomplete = $inputDiv.children('.autocomplete-content.dropdown-content').first();
-        if (!$oldAutocomplete.length) {
-          $inputDiv.append($autocomplete); // Set ul in body
-        }
-      } else {
-        $oldAutocomplete = $input.next('.autocomplete-content.dropdown-content');
-        if (!$oldAutocomplete.length) {
-          $input.after($autocomplete);
-        }
-      }
-      if ($oldAutocomplete.length) {
-        $autocomplete = $oldAutocomplete;
-      }
-
-      // Highlight partial match.
-      var highlight = function(string, $el) {
-        var img = $el.find('img');
-        var matchStart = $el.text().toLowerCase().indexOf("" + string.toLowerCase() + ""),
-            matchEnd = matchStart + string.length - 1,
-            beforeMatch = $el.text().slice(0, matchStart),
-            matchText = $el.text().slice(matchStart, matchEnd + 1),
-            afterMatch = $el.text().slice(matchEnd + 1);
-        $el.html("<span>" + beforeMatch + "<span class='highlight'>" + matchText + "</span>" + afterMatch + "</span>");
-        if (img.length) {
-          $el.prepend(img);
-        }
-      };
-
-      // Reset current element position
-      var resetCurrentElement = function() {
-        activeIndex = 0;
-        $autocomplete.find('.active').removeClass('active');
-      }
-
-      // Perform search
-      $input.off('keyup.autocomplete').on('keyup.autocomplete', function (e) {
-        // Reset count.
-        count = 0;
-
-        // Don't capture enter or arrow key usage.
-        if (e.which === 13 ||
-            e.which === 38 ||
-            e.which === 40) {
-          return;
-        }
-
-        var val = $input.val().toLowerCase();
-
-        // Check if the input isn't empty
-        if (oldVal !== val) {
-          $autocomplete.empty();
-          resetCurrentElement();
-
-          if (val !== '') {
-            fdata = fuse.search(val);
-            for(var key in fdata) {
-                if (count >= options.limit) {
-                  break;
-                }
-                var autocompleteOption = $('<li></li>');
-                autocompleteOption.data("dataSelectionId",dataIdentifierFn(fdata[key]));
-                if (!!renderItemFn) {//!!fuseOptions.key
-                    autocompleteOption.append(renderItemFn(fdata[key]));
+                // Append autocomplete element.
+                // Prevent double structure init.
+                if ($inputDiv.length) {
+                    $oldAutocomplete = $inputDiv.children('.autocomplete-content.dropdown-content').first();
+                    if (!$oldAutocomplete.length) {
+                        $inputDiv.append($autocomplete); // Set ul in body
+                    }
                 } else {
-                  autocompleteOption.append('<span>'+ fdata[key] +'</span>');
+                    $oldAutocomplete = $input.next('.autocomplete-content.dropdown-content');
+                    if (!$oldAutocomplete.length) {
+                        $input.after($autocomplete);
+                    }
+                }
+                if ($oldAutocomplete.length) {
+                    $autocomplete = $oldAutocomplete;
                 }
 
-                $autocomplete.append(autocompleteOption);
-                highlight(val, autocompleteOption);
-                count++;
+                // Highlight partial match.
+                var highlight = function(string, $el) {
+                    var img = $el.find('img');
+                    var matchStart = $el.text().toLowerCase().indexOf("" + string.toLowerCase() + ""),
+                        matchEnd = matchStart + string.length - 1,
+                        beforeMatch = $el.text().slice(0, matchStart),
+                        matchText = $el.text().slice(matchStart, matchEnd + 1),
+                        afterMatch = $el.text().slice(matchEnd + 1);
+                    $el.html("<span>" + beforeMatch + "<span class='highlight'>" + matchText + "</span>" + afterMatch + "</span>");
+                    if (img.length) {
+                        $el.prepend(img);
+                    }
+                };
+
+                // Reset current element position
+                var resetCurrentElement = function() {
+                    activeIndex = 0;
+                    $autocomplete.find('.active').removeClass('active');
+                }
+
+                // Perform search
+                $input.off('keyup.autocomplete').on('keyup.autocomplete', function(e) {
+                    // Reset count.
+                    count = 0;
+
+                    // Don't capture enter or arrow key usage.
+                    if (e.which === 13 ||
+                        e.which === 38 ||
+                        e.which === 40) {
+                        return;
+                    }
+
+                    var val = $input.val().toLowerCase();
+
+                    // Check if the input isn't empty
+                    if (oldVal !== val) {
+                        $autocomplete.empty();
+                        resetCurrentElement();
+
+                        if (val !== '') {
+                            fdata = fuse.search(val);
+                            for (var key in fdata) {
+                                if (count >= options.limit) {
+                                    break;
+                                }
+                                var autocompleteOption = $('<li></li>');
+                                if (!!dataIdentifierFn & typeof(dataIdentifierFn) === "function") {
+                                    autocompleteOption.data("dataSelectionId", dataIdentifierFn(fdata[key]));
+                                }
+                                if (!!renderItemFn & typeof(renderItemFn) === "function") { //!!fuseOptions.key
+                                    autocompleteOption.append(renderItemFn(fdata[key]));
+                                } else {
+                                    autocompleteOption.append('<span>' + fdata[key] + '</span>');
+                                }
+
+                                $autocomplete.append(autocompleteOption);
+                                highlight(val, autocompleteOption);
+                                count++;
+                            }
+                        }
+                    }
+
+                    // Update oldVal
+                    oldVal = val;
+                });
+
+                $input.off('keydown.autocomplete').on('keydown.autocomplete', function(e) {
+                    // Arrow keys and enter key usage
+                    var keyCode = e.which,
+                        liElement,
+                        numItems = $autocomplete.children('li').length,
+                        $active = $autocomplete.children('.active').first();
+
+                    // select element on Enter
+                    if (keyCode === 13) {
+                        liElement = $autocomplete.children('li').eq(activeIndex);
+                        if (liElement.length) {
+                            liElement.click();
+                            e.preventDefault();
+                        }
+                        return;
+                    }
+
+                    // Capture up and down key
+                    if (keyCode === 38 || keyCode === 40) {
+                        e.preventDefault();
+
+                        if (keyCode === 38 &&
+                            activeIndex > 0) {
+                            activeIndex--;
+                        }
+
+                        if (keyCode === 40 &&
+                            activeIndex < (numItems - 1) &&
+                            $active.length) {
+                            activeIndex++;
+                        }
+
+                        $active.removeClass('active');
+                        $autocomplete.children('li').eq(activeIndex).addClass('active');
+                    }
+                });
+                $autocomplete.off();
+                // Set input value
+                $autocomplete.on('click', 'li', function() {
+                    var text = $(this).text().trim();
+                    $input.val(text);
+                    $input.trigger('change');
+                    $input.data("data-id", $(this).data("dataSelectionId"));
+                    $autocomplete.empty();
+                    resetCurrentElement();
+
+                    // Handle onAutocomplete callback.
+                    if (typeof(options.onAutocomplete) === "function") {
+                        options.onAutocomplete.call(this, text);
+                    }
+                });
             }
-          }
-        }
+        });
+    };
 
-        // Update oldVal
-        oldVal = val;
-      });
-
-      $input.off('keydown.autocomplete').on('keydown.autocomplete', function (e) {
-        // Arrow keys and enter key usage
-        var keyCode = e.which,
-            liElement,
-            numItems = $autocomplete.children('li').length,
-            $active = $autocomplete.children('.active').first();
-
-        // select element on Enter
-        if (keyCode === 13) {
-          liElement = $autocomplete.children('li').eq(activeIndex);
-          if (liElement.length) {
-            liElement.click();
-            e.preventDefault();
-          }
-          return;
-        }
-
-        // Capture up and down key
-        if ( keyCode === 38 || keyCode === 40 ) {
-          e.preventDefault();
-
-          if (keyCode === 38 &&
-              activeIndex > 0) {
-            activeIndex--;
-          }
-
-          if (keyCode === 40 &&
-              activeIndex < (numItems - 1) &&
-              $active.length) {
-            activeIndex++;
-          }
-
-          $active.removeClass('active');
-          $autocomplete.children('li').eq(activeIndex).addClass('active');
-        }
-      });
-
-      // Set input value
-      $autocomplete.on('click', 'li', function () {
-        var text = $(this).text().trim();
-        $input.val(text);
-        $input.trigger('change');
-        $input.data("data-id", $(this).data("dataSelectionId"));
-        $autocomplete.empty();
-        resetCurrentElement();
-
-        // Handle onAutocomplete callback.
-        if (typeof(options.onAutocomplete) === "function") {
-          options.onAutocomplete.call(this, text);
-        }
-      });
-    }
-  });
-};
-
-}( jQuery ));
+}(jQuery));
