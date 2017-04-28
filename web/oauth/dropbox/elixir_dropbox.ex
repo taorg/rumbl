f96c0698f08705f8d7ee96693faa2a53cd04536e
iredsausage <taorg@taorgs-MBP-2.Home>
2017-04-28 16:21:26 +0200

    Ya se hace a cachos

--- web/oauth/dropbox/elixir_dropbox.ex
+++ web/oauth/dropbox/elixir_dropbox.ex
@@ -3,7 +3,7 @@ defmodule ElixirDropbox do
   ElixirDropbox is a wrapper for Dropbox API V2
   """
   use HTTPoison.Base
-
+  require Logger
   @type response :: {any}
 
   @base_url_v1 "https://api.dropboxapi.com/1"
@@ -62,8 +62,40 @@ defmodule ElixirDropbox do
     post_request(client, "#{@upload_url}#{url}", {:file, data}, headers)
   end
 
-  def download_request(client, url, data, headers) do
+  def download_request1(client, url, data, headers) do
     headers = Map.merge(headers, headers(client))
     HTTPoison.post!("#{@upload_url}#{url}", data, headers) |> download_response
+  end
+
+  def download_request(client, url, data, headers) do
+    headers = Map.merge(headers, headers(client))
+    case HTTPoison.post!("#{@upload_url}#{url}", data, headers, [stream_to: self(), async: :once]) do
+      resp = %HTTPoison.AsyncResponse{id: id} ->
+        receive do
+              %HTTPoison.AsyncStatus{ id: ^id, code: status } ->
+                case status do
+                  200 -> {:ok, async_loop(id, resp, %{file: <<>>})}
+                  non_200 -> {:error, non_200}
+                end
+              whatever -> {:error, whatever}
+            end
+      whatever -> Logger.debug "#{inspect whatever}"
+    end
+ 
+  end
+  
+  def async_loop id, resp, acc do
+    {:ok, ^resp} = HTTPoison.stream_next(resp)
+    receive do
+      %HTTPoison.AsyncHeaders{ id: ^id, headers: headers } ->
+        async_loop(id, resp, Map.put(acc, :headers, (get_header(headers, "dropbox-api-result") |> Poison.decode)))
+ 
+      %HTTPoison.AsyncChunk{ id: ^id, chunk: chunk } ->
+        async_loop(id, resp, Map.update(acc, :file, <<>>, &(&1 <> chunk)))
+ 
+      %HTTPoison.AsyncEnd{ id: ^id } -> acc
+ 
+      whatever -> Logger.debug "#{inspect whatever}"
+    end
   end
 end
